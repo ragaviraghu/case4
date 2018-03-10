9735c087cb88e5e231623a086a60f20ab550eda5
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-10 09:36:17 -0800

    Create spaces.cpp

---
+++ spaces.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string s;
+int len,i,count=0;
+cout<<"Enter the string : "<<endl;
+getline(cin,s);
+len=s.size();
+for(i=0;i<len;i++)
+{
+if(s[i]==' ')
+{
+ count++;
+}
+}
+cout<<"Number of spaces in the given string are : "<<count<<endl;
+return 0;
+}
